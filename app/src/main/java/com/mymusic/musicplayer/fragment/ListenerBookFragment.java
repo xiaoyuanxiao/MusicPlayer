79f6b814b2780b3962965d17915be2c1cbf4d7f6
xiaoyu <2640171401@qq.com>
2017-11-15 11:06:41 +0800

    轮播图

--- app/src/main/java/com/mymusic/musicplayer/fragment/ListenerBookFragment.java
+++ app/src/main/java/com/mymusic/musicplayer/fragment/ListenerBookFragment.java
@@ -3,12 +3,20 @@ package com.mymusic.musicplayer.fragment;
 import android.view.View;
 
 import com.mymusic.musicplayer.R;
+import com.mymusic.musicplayer.view.GlideImageLoader;
+import com.youth.banner.Banner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by xiaoyu on 2017/11/15.
  */
 
 public class ListenerBookFragment extends BaseFragment {
+    private List images = new ArrayList<>();
+    private Banner banner;
+
     public static ListenerBookFragment newInstance() {
         ListenerBookFragment listenerBookFragment = new ListenerBookFragment();
         return listenerBookFragment;
@@ -17,8 +25,29 @@ public class ListenerBookFragment extends BaseFragment {
     @Override
     View initView() {
         View inflate = View.inflate(getContext(), R.layout.fragment_listenerbook, null);
+        banner = (Banner) inflate.findViewById(R.id.banner);
         return inflate;
     }
+
+    @Override
+    protected void initData() {
+        super.initData();
+
+        images.add("http://jyts-public-oss.longruncloud.com/carousel/2017/07/05/7a55c7fd9e6cafe15cd9301c838ace8689598b1cbbabc319a63af7489bea659c.jpg");
+        images.add("http://jyts-public-oss.longruncloud.com/carousel/2017/07/05/7a55c7fd9e6cafe15cd9301c838ace8689598b1cbbabc319a63af7489bea659c.jpg");
+        images.add("http://jyts-public-oss.longruncloud.com/carousel/2017/07/05/7a55c7fd9e6cafe15cd9301c838ace8689598b1cbbabc319a63af7489bea659c.jpg");
+        images.add("http://jyts-public-oss.longruncloud.com/carousel/2017/07/05/7a55c7fd9e6cafe15cd9301c838ace8689598b1cbbabc319a63af7489bea659c.jpg");
+
+        //设置图片加载器
+        banner.setImageLoader(new GlideImageLoader());
+        // 设置图片集合
+        banner.setImages(images);
+        //banner设置方法全部调用完毕时最后调用
+        banner.start();
+
+
+    }
+
 
     @Override
     protected void setOnclick() {
