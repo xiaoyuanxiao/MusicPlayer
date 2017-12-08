eb007f61795586f68f6dd3e5acee29f6a81c6921
deepcummer <750264859@qq.com>
2017-12-08 13:41:48 +0800

    BaseFragment

---
+++ app/src/main/java/com/mymusic/musicplayer/bean/AudioUrlBean.java
@@ -0,0 +1,23 @@
+package com.mymusic.musicplayer.bean;
+
+/**
+ * Created by xiaoyu on 2017/12/8.
+ */
+
+public class AudioUrlBean {
+
+
+    /**
+     * url : http://jyts-public-oss.longruncloud.com/audios/94e5ea80a3c8ea983d14c35e8b003ed2d90a6fc65662c8e48f4e61500a124d1f.mp3
+     */
+
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
